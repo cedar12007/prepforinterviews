dc648a377e357b4ff351bdd2f40cecb94955ef41
cedar12007 <97471268+cedar12007@users.noreply.github.com>
2025-01-17 12:50:41 -0500

    Updating backend code to use CORS

--- index.py
+++ index.py
@@ -1,45 +1,16 @@
-from flask import Flask, request, jsonify, send_from_directory
-import os
-
+from flask import Flask, request, jsonify
+from flask_cors import CORS  # Import CORS library
 import requests
-#from flask_cors import CORS  # Import CORS
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
-@app.route('/')
-def serve_html():
-    # Serve the index.html file directly from the root directory
-    return send_from_directory(os.getcwd(), 'index.html')
-
-#CORS(app)  # Enable CORS for the whole app
-# Enable CORS for the entire app
-#CORS(app, origins=["http://127.0.0.1:5500", "http://localhost:5500", "http://localhost:63342/", "https://www.prepforinterview.com/"])  # Allow specific origin(s)
-# Define the allowed origins
-allowed_origins = ["http://127.0.0.1:5500", "http://localhost:5500", "http://localhost:63342/", "https://www.prepforinterview.com/"]
-
-@app.after_request
-def after_request(response):
-    origin = request.headers.get('Origin')
-
-    if origin in allowed_origins:
-        response.headers['Access-Control-Allow-Origin'] = origin
-        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
-        response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
-        response.headers['Access-Control-Allow-Credentials'] = 'true'
-
-    # Handle OPTIONS request (pre-flight request)
-    if request.method == 'OPTIONS':
-        response.status_code = 200
-    return response
-
-# Replace with your reCAPTCHA secret key
 RECAPTCHA_SECRET_KEY = "6LcXxroqAAAAAGeX9BkQ5oAxyKeeyoGPpesYUQkL"
 
-@app.route('/validate-captcha', methods=['POST'])
+@app.route("/validate-captcha", methods=["POST"])
 def validate_captcha():
-    print("request: " + str(request.get_json()))
-
     data = request.json
+    print("validate-captcha data: " + str(data))
     recaptcha_response = data.get("g-recaptcha-response")
 
     # Verify reCAPTCHA response
@@ -52,14 +23,10 @@ def validate_captcha():
     )
     result = response.json()
 
-    if result.get("success") and result.get("score", 0) >= 0.5:  # Check score for v3
-        # Process form data here
+    if result.get("success"):
         return jsonify({"success": True, "message": "CAPTCHA validation successful."})
     else:
         return jsonify({"success": False, "message": "CAPTCHA validation failed."}), 400
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
-
-#CORS(app, resources={r"/validate-captcha": {"origins": "https://yourusername.github.io"}})
