ddae4d15b109621810b2e3db08501ff811bd354a
cedar12007 <97471268+cedar12007@users.noreply.github.com>
2025-01-17 15:29:43 -0500

    added addedupstash-redis==1.0.0 to requirements.txt

--- redit_test.py
+++ redit_test.py
@@ -1,6 +1,6 @@
 from upstash_redis import Redis
 
-redis = Redis(url="https://tops-sparrow-26652.upstash.io", token="AWgcAAIjcDFlMzk4NzI1MTQ0MDA0NTY0ODRlNjMwN2Y3ZDJhMDcwZXAxMA")
+redis = Redis(url="https://tops-sparrow-26652.upstash.io", token="xxx")
 
 redis.set("foo", "bar")
 value = redis.get("foo")
